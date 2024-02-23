40c3ca6426b5570d8d9f0da5e8c2f7e6bc2ee22c
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-02-24 00:54:24 +0300

    fix: syntax error

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
+# creates a file in /tmp using Puppet
+
 file { '/tmp/school':
-  ensure  => file,
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+    ensure  => file,
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet'
 }
