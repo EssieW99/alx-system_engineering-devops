d9fc81156ca7bf8cdd6af18d944452a1cf6a4f3d
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-05-09 05:15:22 +0300

    0. Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -6,3 +6,8 @@ file { '/var/www/html/wp-includes/class-wp-locale.phpp':
 file { '/var/www/html/wp-includes/class-wp-locale.php':
   ensure => present,
 }
+
+exec { 'rename_file':
+  command => 'mv /var/www/html/wp-includes/class-wp-locale.phpp /var/www/html/wp-includes/class-wp-locale.php',
+  unless  => 'test -f /var/www/html/wp-includes/class-wp-locale.php',
+}
