efdce20c59be3dce6c783119ee83036732311729
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-05-09 05:36:25 +0300

    0. Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # edits an incorrect file name
 
 exec { 'rename_file':
-  command => 'mv /var/www/html/wp-includes/class-wp-locale.phpp /var/www/html/wp-includes/class-wp-locale.php',
-  onlyif  => 'test -e /var/www/html/wp-includes/class-wp-locale.phpp',
+  command => 'sed -i "s/.phpp/.php /var/www/html/wp-settings.php',
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
 }
