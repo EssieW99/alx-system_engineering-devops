65c3f52d52f761247fa70e4d420927bd1f58f9bb
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-05-09 05:38:02 +0300

    correction

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # edits an incorrect file name
 
 exec { 'rename_file':
-  command => 'sed -i "s/.phpp/.php /var/www/html/wp-settings.php',
+  command => 'sed -i "s/.phpp/.php" /var/www/html/wp-settings.php',
+
   onlyif  => 'test -e /var/www/html/wp-settings.php',
 }
