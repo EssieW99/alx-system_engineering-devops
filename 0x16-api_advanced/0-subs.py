1914619fa47ae467adb1968beb09d173d8d2c92d
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-05-10 04:08:08 +0300

    0. How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+returns the number of subscribers for a given subreddit
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    returns the number of subscribers for a given subreddit
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'MyAPI/7.68.0'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
